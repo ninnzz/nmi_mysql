f3fb7385efc86a31470d3c77a41596e9ba57a017
pprmint <nreclarin@gmail.com>
2015-09-17 20:15:30 +0800

    package updated

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Setup file
+"""
+from distutils.core import setup
+
+
+setup(
+    name='nmi_mysql',
+    packages=['nmi_mysql'],
+    version='0.4',
+    description='A simple mysql client wrapper for pymysql',
+    author='Ninz',
+    author_email='ninz.xp@gmail.com',
+    url='https://github.com/pprmint/nmi_mysql/tarball/0.4',
+    keywords=['mysql', 'client', 'wrapper', 'library', 'pymysql'],
+    classifiers=[],
+)
