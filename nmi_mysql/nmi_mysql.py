18b82fe5bd9a91356c5e81be4b09e590fb6fa533
jvmsangkal <johnviscelsangkal@gmail.com>
2015-10-21 11:04:58 +0800

    add comments and implement is_empty

--- nmi_mysql/nmi_mysql.py
+++ nmi_mysql/nmi_mysql.py
@@ -7,7 +7,7 @@ import re
 import logging
 import pymysql.cursors
 
-from multiprocessing import Queue
+from queue import Queue
 
 
 class DB():
@@ -132,14 +132,20 @@ class DB():
 
 
 class ConnectionPool():
+    """
+    Usage:
+        conn_pool = nmi_mysql.ConnectionPool(config)
 
+        db = conn_pool.get_connection()
+        db.query('SELECT 1', [])
+        conn_pool.return_connection(db)
+
+        conn_pool.close()
+    """
     def __init__(self, conf, max_pool_size=20):
         self.conf = conf
         self.max_pool_size = max_pool_size
         self.initialize_pool()
-
-    def get_initialized_connection_pool(self):
-        return self.pool
 
     def initialize_pool(self):
         self.pool = Queue(maxsize=self.max_pool_size)
@@ -147,8 +153,10 @@ class ConnectionPool():
             self.pool.put_nowait(DB(self.conf, True))
 
     def get_connection(self):
-        # returns a db instance when one is available
+        # returns a db instance when one is available else waits until one is
         db = self.pool.get(True)
+
+        # checks if db is still connected because db instance automatically closes when not in used
         if not self.ping(db):
             db.connect()
 
@@ -157,10 +165,16 @@ class ConnectionPool():
     def return_connection(self, db):
         return self.pool.put_nowait(db)
 
-    def close_pool(self):
-        while not self.pool.empty():
+    def close(self):
+        while not self.is_empty():
             self.pool.get().close()
 
     def ping(self, db):
         data = db.query('SELECT 1', [])
         return data
+
+    def get_initialized_connection_pool(self):
+        return self.pool
+
+    def is_empty(self):
+        return self.pool.empty()
